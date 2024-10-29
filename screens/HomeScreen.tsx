c3ae6dd06cea0ef5431aac376e3545d5638dc4af
meshalTitum97 <meshal@titumlk.onmicrosoft.com>
2024-10-29 09:37:04 +0530

    Update HomeScreen.tsx

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,9 +1,10 @@
 /* eslint-disable prettier/prettier */
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { Text, View } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import { Text, TextInput, View } from 'react-native';
 import * as Icon from 'react-native-feather';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { themeColors } from 'theme';
 
 const HomeScreen = () => {
   return (
@@ -12,6 +13,14 @@ const HomeScreen = () => {
       <View className="flex-row items-center px-4 pb-2 space-x-2">
         <View className="flex-row items-center flex-1 p-3 border border-gray-300 rounded-full">
           <Icon.Search width="25" height="25" stroke="gray" />
+          <TextInput placeholder="Restaurent" className="flex-1 ml-4" />
+          <View className="flex-row items-center pl-2 space-x-1 border-0 border-l-2 border-l-gray-300">
+            <Icon.MapPin height="20" width="20" stroke="gray" />
+            <Text className="ml-1 text-gray-500">New York, NYC</Text>
+          </View>
+        </View>
+        <View style={{ backgroundColor: themeColors.bgColor(1) }} className="p-3 ml-2 rounded-full">
+          <Icon.Sliders width="20" height="20" strokeWidth={2.5} stroke="white" />
         </View>
       </View>
     </SafeAreaView>
