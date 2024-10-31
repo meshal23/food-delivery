f5467998051c0de60bd58ce2f6955f962b0e2681
meshalTitum97 <meshal@titumlk.onmicrosoft.com>
2024-10-31 13:38:08 +0530

    Update App.tsx
    included react query

--- App.tsx
+++ App.tsx
@@ -1,9 +1,15 @@
 import './global.css';
-
 import 'react-native-gesture-handler';
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
 
 import Navigation from './navigation';
 
+const queryClient = new QueryClient({});
+
 export default function App() {
-  return <Navigation />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Navigation />
+    </QueryClientProvider>
+  );
 }
